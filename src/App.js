1264adcc781c3c22a097019df440e031bb40aa9b
aanchalsheth31 <asheth@betterworldxdesign.com>
2022-11-10 19:01:26 -0500

    lab done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -12,16 +13,49 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  const [cartTotal, setCount] = useState(0); // setcount is the updating function
+  const [cartTotalItems, setCountItems] = useState(0); // setcount is the updating function
+  const [nameCartItems, addItemName] = useState([]);
+  
+  function addToCart(price, name) {
+    //  setCount(cartTotal + price);
+    //  setCountItems(cartTotalItems + 1);
+    // nameCartItems = [name];
+
+    //  addItemName(nameCartItems,...name);
+
+
+    // addItem([...items, { name: name, count: count}]);
+    //add an array of strings
+    // cartTotal = cartTotal + price;
+}
+    
+
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem name={item.name} description={item.description} price={item.price} image={item.image}
+          cartTotal={cartTotal} cartTotalItems={cartTotalItems} nameCartItems={nameCartItems}
+          setCount={setCount} setCountItems={setCountItems} addItemName={addItemName} />
+        
       ))}
 
       <div>
         <h2>Cart</h2>
+        {/* <div className="cardItem">
+         
+        </div> */}
+        <p>Total price of the cart:</p>
+        {cartTotal}
+        <p>No. of items in the cart:</p>
+        {cartTotalItems}
+        <p> Items Added:</p>
+        {nameCartItems.map((item) => (
+              <p>{item.name}</p>
+          ))}
+        {/* <p>{ nameCartItems[0]}</p> */}
         {/* TODO: render a list of items in the cart */}
       </div>
     </div>
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,31 @@
 // TODO: create a component that displays a single bakery item
+import { useState } from "react";
+export default function BakeryItem({ name, description, price, image,
+    cartTotal, cartTotalItems, nameCartItems,
+    setCount, setCountItems, addItemName }) {
+    //const [count, setCount] = useState(0);
+    // const [cartTotal, setCount] = useState(0); // setcount is the updating function
+    // const [cartTotalItems, setCountItems] = useState(0); // setcount is the updating function
+    // 
+    function updateCart(price, name) {
+        setCountItems(cartTotalItems + 1);
+        setCount(cartTotal + price);
+        addItemName([...nameCartItems, { name: name, count: cartTotal}]);
+        
+        // addItemName(nameCartItems.push(name));
+    }
+    
+    return (
+     <div className="itemBakery">
+            <p>Bakery Item <br></br>
+            <img src= {image} class="images"></img><br></br>
+               Name: {name} <br></br>
+                About: {description}<br></br>
+                Price: { price}<br></br>
+                <button onClick={() => updateCart(price, name)}>Add to Cart</button>
+                
+        </p>
+        </div>
+    );
+}
+  
